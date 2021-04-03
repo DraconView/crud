9cb095701dd28334e2f3548cabcff5e7980583d5
DraconView <nemines002@gmail.com>
2021-04-03 16:55:02 -0500

    mi primer commit

---
+++ crud/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="container mt-5" >
+    <h1>
+      TAREAS
+      <hr/> 
+    </h1>     
+  </div>
+  );
+}
+
+export default App;
