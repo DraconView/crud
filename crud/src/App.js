f77799db91ff0133274e93b6c2452d38a08c57ba
DraconView <nemines002@gmail.com>
2021-04-04 17:32:30 -0500

    Tutorial 64 - React / React-Native - 14 - Validando el ingreso de una tarea

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,13 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
+ //hook de estado sirve para almacenar datos y poder modificarlos
+import { isEmpty } from 'lodash'
 
-function App() {
-  return (
+function App() {  
+  const [task, setTask] = useState("")
+ //este useState va tener 3 valores (task es el nombre, setTask se va llamar el motodo que modofica, va iniciar en null)  
+
+   const addTask = (e) => {
+    e.preventDefault()
+    if (isEmpty(task)) {
+    console.log("Task empty")
+    return
+    }
+    console.log("Ok")
+    setTask("")
+  }
+ 
+ return (
     <div className="container mt-5" >
-    <h1>
-      envia un mensaje de vuelta a mi wasap con el codigo 4463 o 2355 o 6589 estoy haciendo unas pruebas
+    <h1> TAREAS </h1>
       <hr/> 
-    </h1>     
-  </div>
+      <div className="row" >
+      <div className="col-8" >
+        <h4 className="text-center"> lista de tareas </h4>
+        <ul className="list-group"> 
+        <li className="list-group-item">
+          <span className="lead">Nombre de la tarea</span>
+          <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+          <button className="btn btn-warning btn-sm float-right">Editar</button>
+        </li>
+        </ul>
+        </div>
+        <div className="col-4">
+        <h4 className="text-center">Formulario</h4>
+        <form onSubmit={addTask}>
+      <input
+      type="text"
+      className="form-control mb-2"
+      placeholder="Ingrese la tarea..."
+      onChange={(text)=>setTask(text.target.value)}
+      value={task}
+      />
+      <button
+      className="btn btn-dark btn-block"
+      type="submit"
+      >
+        AGREGAR
+      </button>
+      </form>
+      </div>
+    </div>
+    </div>
   );
 }
 
