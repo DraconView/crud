b45b8601a4cc69e7637711c63e5daf187e574230
DraconView <nemines002@gmail.com>
2021-04-05 22:18:11 -0500

    Tutorial 64 - React / React-Native - 15 - Adicionando una tarea y mostrarla en la lista de tareas

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,18 +1,24 @@
-import React, {useState} from 'react'
- //hook de estado sirve para almacenar datos y poder modificarlos
+import React, {useState} from 'react'  //hook de estado sirve para almacenar datos y poder modificarlos
 import { isEmpty } from 'lodash'
+import shortid from 'shortid'
 
 function App() {  
-  const [task, setTask] = useState("")
- //este useState va tener 3 valores (task es el nombre, setTask se va llamar el motodo que modofica, va iniciar en null)  
-
-   const addTask = (e) => {
+  const [task, setTask] = useState("") //este useState va tener 3 valores (task es el nombre, setTask se va llamar el motodo que modofica, va iniciar en null)  
+  const [tasks, setTasks] = useState([])
+   
+  const addTask = (e) => {
     e.preventDefault()
     if (isEmpty(task)) {
     console.log("Task empty")
     return
     }
-    console.log("Ok")
+
+    const newTask = {
+      id: shortid.generate(), //genera un codigo alfanumerico que no se repite 
+      name: task
+    }
+
+    setTasks([...tasks, newTask])
     setTask("")
   }
  
@@ -21,35 +27,39 @@ function App() {
     <h1> TAREAS </h1>
       <hr/> 
       <div className="row" >
-      <div className="col-8" >
-        <h4 className="text-center"> lista de tareas </h4>
-        <ul className="list-group"> 
-        <li className="list-group-item">
-          <span className="lead">Nombre de la tarea</span>
-          <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-          <button className="btn btn-warning btn-sm float-right">Editar</button>
-        </li>
-        </ul>
+        <div className="col-8" >
+          <h4 className="text-center"> lista de tareas </h4>
+          <ul className="list-group"> 
+           {
+              tasks.map((task) => ( 
+            <li className="list-group-item" key={task.id}>
+              <span className="lead">{task.name}</span>
+              <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+              <button className="btn btn-warning btn-sm float-right">Editar</button>
+            </li>
+                ))
+            }
+          </ul>
         </div>
         <div className="col-4">
-        <h4 className="text-center">Formulario</h4>
-        <form onSubmit={addTask}>
-      <input
-      type="text"
-      className="form-control mb-2"
-      placeholder="Ingrese la tarea..."
-      onChange={(text)=>setTask(text.target.value)}
-      value={task}
-      />
-      <button
-      className="btn btn-dark btn-block"
-      type="submit"
+          <h4 className="text-center">Formulario</h4>
+          <form onSubmit={addTask}>
+        <input
+        type="text"
+        className="form-control mb-2"
+        placeholder="Ingrese la tarea..."
+        onChange={(text)=>setTask(text.target.value)}
+        value={task}
+        />
+          <button
+        className="btn btn-dark btn-block"
+        type="submit"
       >
         AGREGAR
-      </button>
-      </form>
+          </button>
+        </form>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
