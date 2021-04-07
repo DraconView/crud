91bf0b43b0751c5ced3b678c681654e08526bd80
DraconView <nemines002@gmail.com>
2021-04-06 21:09:36 -0500

    Tutorial 64 - React / React-Native - 16 -
     Eliminar una tarea

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'  //hook de estado sirve para almacenar datos y poder modificarlos
-import { isEmpty } from 'lodash'
+import { isEmpty, size } from 'lodash'
 import shortid from 'shortid'
 
 function App() {  
@@ -21,6 +21,11 @@ function App() {
     setTasks([...tasks, newTask])
     setTask("")
   }
+
+  const deleteTask = (id) => {
+    const filteredTasks = tasks.filter(task => task.id !== id)
+    setTasks(filteredTasks)
+    }
  
  return (
     <div className="container mt-5" >
@@ -29,17 +34,33 @@ function App() {
       <div className="row" >
         <div className="col-8" >
           <h4 className="text-center"> lista de tareas </h4>
-          <ul className="list-group"> 
+      {
+        size(tasks) === 0 ? (
+          <h5 className="text-center">Aun no hay tareas programadas.</h5>
+        ) : (
+            <ul className="list-group"> 
            {
               tasks.map((task) => ( 
             <li className="list-group-item" key={task.id}>
               <span className="lead">{task.name}</span>
-              <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-              <button className="btn btn-warning btn-sm float-right">Editar</button>
+              <button
+               className="btn btn-danger btn-sm float-right mx-2"
+               onClick={() => deleteTask(task.id)}
+               >
+                Eliminar
+              </button>
+              <button
+               className="btn btn-warning btn-sm float-right"
+              >
+                Editar
+              </button>
             </li>
                 ))
             }
           </ul>
+        )
+
+       }
         </div>
         <div className="col-4">
           <h4 className="text-center">Formulario</h4>
